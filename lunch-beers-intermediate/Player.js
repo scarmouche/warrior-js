9749df4320d3f680e6759182fc1c11ff483f2a1d
Selena Carmouche <scarmouche@dustland.com>
2015-05-25 23:48:17 -0700

    Level 3

--- lunch-beers-intermediate/Player.js
+++ lunch-beers-intermediate/Player.js
@@ -1,6 +1,39 @@
 class Player {
   playTurn(warrior) {
-    // Cool code goes here
+   
+    var stairDirection = warrior.directionOfStairs(),
+        enemies = [];
+
+    findNearbyEnemies();
+
+    if (enemies.length) {
+      if (multipleEnemies()) {
+        warrior.bind(enemies[0]);
+      } else {
+        warrior.attack(enemies[0]);
+      }   
+    } else if (warrior.feel(stairDirection).isEnemy()) {
+      warrior.attack(stairDirection);
+    } else {
+      warrior.walk(stairDirection);
+    } 
+ 
+    function findNearbyEnemies() {
+      /* Right now this code only works with the directions in this order */
+      var directions = ['forward', 'backward', 'left', 'right'];
+
+      for (var i = 0; i < directions.length; i++) {
+        var dir = directions[i];
+        if (warrior.feel(dir).isEnemy()) {
+          enemies.push(dir);
+        }
+      }
+    }
+
+    function multipleEnemies() {
+      return enemies.length >= 2;
+    }
+
   }
 }
 
