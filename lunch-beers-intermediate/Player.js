36046b086d9515029a1686bc1962d2bf45d92779
Selena Carmouche <scarmouche@dustland.com>
2015-05-25 23:44:53 -0700

    Level 2

--- lunch-beers-intermediate/Player.js
+++ lunch-beers-intermediate/Player.js
@@ -1,6 +1,29 @@
+// level 2
+
 class Player {
   playTurn(warrior) {
-    // Cool code goes here
+   
+    var HP = warrior.health(),
+        stairDirection = warrior.directionOfStairs();
+
+    if ( warrior.feel(stairDirection).isEmpty() && HP < 8) {
+      warrior.rest();
+    } else if ( warrior.feel(stairDirection).isEmpty()) {
+      warrior.walk(stairDirection);
+    } else {
+      attackEnemies();
+    }
+
+    function attackEnemies() {
+      var directions = ['forward', 'left', 'right', 'backward'];
+      for (var i = 0; i < directions.length; i++) {
+        var dir = directions[i];
+        if (warrior.feel(dir).isEnemy()) {
+          warrior.attack(dir);
+        }
+      }
+    }
+    
   }
 }
 
