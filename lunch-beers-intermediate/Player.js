a0c1387ee0322c561c2c59b1d4bc6980057ca3d5
Selena Carmouche <scarmouche@dustland.com>
2015-05-25 23:50:41 -0700

    Level 4

--- lunch-beers-intermediate/Player.js
+++ lunch-beers-intermediate/Player.js
@@ -1,6 +1,47 @@
+// level 4
+
 class Player {
   playTurn(warrior) {
-    // Cool code goes here
+   
+    var stairDirection = warrior.directionOfStairs(),
+        occupiedSpaces = warrior.listen(),
+        enemies = [],
+        captives = [];
+
+        analyzeFloor();
+
+        if (captives.length) {
+          rescueCaptives();
+        } else if (warrior.feel(stairDirection).isEmpty()) {
+          warrior.walk(stairDirection);
+        } else {
+          warrior.attack(stairDirection);
+        }
+
+        function analyzeFloor() {
+          for (var i = 0; i < occupiedSpaces.length; i++) {
+            var space = occupiedSpaces[i];
+            if (space.isEnemy()) {
+              enemies.push(space);
+            } else if (space.isCaptive()) {
+              captives.push(space);
+            } 
+          }
+        }
+
+        function rescueCaptives() {
+          for (var i = 0; i < captives.length; i++) {
+            var dir = warrior.directionOf(captives[i]);
+            if (warrior.feel(dir).isCaptive()) {
+              return warrior.rescue(dir);
+            } else if (warrior.feel(dir).isEmpty()) {
+              return warrior.walk(dir);
+            } else {
+              return warrior.attack(dir);
+            }
+          }
+        }
+
   }
 }
 
